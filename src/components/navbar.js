4b8307783cd8619963f299b542d302d06d0cf97d
Yiti Jiang <yiti.jiang@gmail.com>
2020-12-26 23:50:49 -0500

    refactor navbar to use button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,9 @@
 import React from "react"
-const Navbar = ({ children, ...props }) => {
+const Navbar = ({ navigate }) => {
   return (
     <div className="fixed inset-x-0 bottom-0 justify-center flex flex-row z-50">
-      {children}
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/about")}>About</button>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,46 @@
 import React from "react"
 import Navbar from "../components/navbar"
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import { TransitionGroup, Transition } from "react-transition-group"
 import "../page-animations.css"
 import "./index.css"
 
-const MainLinks = () => {
-  return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </>
-  )
-}
 const Home = props => <div className="text-center mt-96">Home</div>
 
 const About = props => <div className="text-center mt-96">About</div>
 
-const IndexPage = () => (
-  <BrowserRouter>
-    <Navbar>
-      <MainLinks />
-    </Navbar>
-    <Route
-      render={({ location }) => {
-        const { pathname, key } = location
-        return (
-          <div className="page-wrapper">
-            <TransitionGroup component={null}>
-              <Transition key={pathname} timeout={2000}>
-                {state => {
-                  let animClass = ""
-                  if (state === "entering") {
-                    animClass = "rotate-right-enter"
-                  }
-                  if (state === "exiting") {
-                    animClass = "rotate-right-exit"
-                  }
-                  return (
-                    <div className={"bg-gray-300 page " + animClass}>
-                      <Switch location={location}>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
-                      </Switch>
-                    </div>
-                  )
-                }}
-              </Transition>
-            </TransitionGroup>
-          </div>
-        )
-      }}
-    />
-  </BrowserRouter>
-)
+const IndexPage = () => {
+  const [page, setPage] = React.useState("/")
+  return (
+    <>
+      <Navbar navigate={setPage} />
+      <div className="page-wrapper">
+        <TransitionGroup component={null}>
+          <Transition key={page} timeout={2000}>
+            {state => {
+              let animClass = ""
+              if (state === "entering") {
+                animClass = "rotate-right-enter"
+              }
+              if (state === "exiting") {
+                animClass = "rotate-right-exit"
+              }
+              let comp
+              switch (page) {
+                case "/":
+                  comp = <Home />
+                  break
+                case "/about":
+                  comp = <About />
+                  break
+              }
+              return (
+                <div className={"bg-gray-300 page " + animClass}>{comp}</div>
+              )
+            }}
+          </Transition>
+        </TransitionGroup>
+      </div>
+    </>
+  )
+}
 export default IndexPage
