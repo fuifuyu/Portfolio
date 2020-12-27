09b4fd651784a31e2354a3b81fa0ec15f26f215f
Yiti Jiang <yiti.jiang@gmail.com>
2020-12-27 01:02:19 -0500

    control animation with preset

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import { TransitionGroup, Transition } from "react-transition-group"
+import { roomMap, animPresetMap } from "../maps/animationMap"
 import "../page-animations.css"
 import "./index.css"
 
@@ -8,21 +9,29 @@ const Home = props => <div className="text-center mt-96">Home</div>
 
 const About = props => <div className="text-center mt-96">About</div>
 
+let nextPage = "/"
+let curPage = "/"
+
 const IndexPage = () => {
   const [page, setPage] = React.useState("/")
+  function navigate(key) {
+    curPage = page
+    nextPage = key
+    setPage(key)
+  }
   return (
     <>
-      <Navbar navigate={setPage} />
+      <Navbar navigate={navigate} />
       <div className="page-wrapper">
         <TransitionGroup component={null}>
-          <Transition key={page} timeout={2000}>
+          <Transition key={page} timeout={800}>
             {state => {
               let animClass = ""
-              if (state === "entering") {
-                animClass = "rotate-right-enter"
-              }
-              if (state === "exiting") {
-                animClass = "rotate-right-exit"
+              if (state == "entering" || state == "exiting") {
+                let preset = roomMap[curPage][nextPage]
+                if (preset) {
+                  animClass = animPresetMap[preset][state]
+                }
               }
               let comp
               switch (page) {
