83683b376cb0af2fd59be6b3c01f7788f57fec89
Yiti Jiang <yiti.jiang@gmail.com>
2021-01-01 03:20:13 -0500

    hoverable navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,19 @@
 import React from "react"
+import "./components.css"
 const Navbar = ({ navigate }) => {
   return (
-    <div className="fixed inset-x-0 bottom-0 justify-center flex flex-row z-50">
-      <button onClick={() => navigate("intro")}>Intro</button>
-      <button onClick={() => navigate("skill")}>Skill</button>
-      <button onClick={() => navigate("timeline")}>Timeline</button>
-      <button onClick={() => navigate("goal")}>Goal</button>
-      <button onClick={() => navigate("gallery")}>Gallery</button>
-    </div>
+    <>
+      <div
+        tabIndex="-1"
+        className="fixed inset-x-0 bottom-0 justify-center items-end flex flex-row z-50 hoverable pb-4"
+      >
+        <button onClick={() => navigate("intro")}>Intro</button>
+        <button onClick={() => navigate("skill")}>Skill</button>
+        <button onClick={() => navigate("timeline")}>Timeline</button>
+        <button onClick={() => navigate("goal")}>Goal</button>
+        <button onClick={() => navigate("gallery")}>Gallery</button>
+      </div>
+    </>
   )
 }
 
