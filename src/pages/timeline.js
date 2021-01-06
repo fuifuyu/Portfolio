5388ea4f4bf4d7b4c7b4c5850913abf92e53e560
Yiti Jiang <yiti.jiang@gmail.com>
2021-01-06 17:14:57 -0500

    timeline added

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -5,14 +5,14 @@ export default function Timeline({ className }) {
     <div className={className + " bg-timeline"}>
       <div
         className="relative w-1/2 h-1/2 mx-auto"
-        style={{ overflowX: "auto" }}
+        style={{ overflowX: "auto", overflowY: "hidden" }}
         onWheel={e => {
           console.log(e)
           e.currentTarget.scrollLeft += e.deltaY
           e.stopPropagation()
         }}
       >
-        <div className="absolute-center-y flex flex-row text-xl text-white flex-nowrap">
+        <div className="absolute-center-y flex flex-row text-xl text-white flex-nowrap h-48">
           <div class="card">test</div>
           <div class="card">test</div>
           <div class="card">test</div>
